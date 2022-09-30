4e4414f875bca40bde5e0d578158f7b49f3c388a
dgdcosgrove <dgdcosgrove@gmail.com>
2022-09-30 02:45:50 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! you got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -70,12 +72,22 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get the current score from the dom and increments it by 1
+ */
 function incrementScore() {
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+}
+/**
+ * Get the current tally of incorrect answers from the dom and increments it by 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
